d8d4be56f267779b05adadd7b7ac0ab837eaad9f
Gias uddin vuiya <giasuddinvuiya2022@gmail.com>
2023-08-24 20:19:29 +0600

    delete-fetures/added

--- src/PostLists.jsx
+++ src/PostLists.jsx
@@ -1,10 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import AddPost from "./component/AddPost"; 
 import { fetchPosts } from "./api/posts";
 import { useNavigate } from "react-router-dom";
-
+import { deletePost } from "./api/posts";
 function PostList()
 {
+    const queryClient = useQueryClient(); 
     const navigate = useNavigate()
     // were getting data form json
     const {isLoading, data : posts, isError, error,} = useQuery({
@@ -12,7 +13,22 @@ function PostList()
         queryFn : fetchPosts
     }); 
     
-    // 
+    // DELETE
+    const deletePostMutation = useMutation({
+        mutationFn : deletePost,
+        onSuccess : () => {
+            queryClient.invalidateQueries({queryKey : ["posts"]}); 
+            console.log('successfully delete')
+            navigate("/");
+          }
+    })
+
+    const handleDelete = (id) => 
+    {
+        deletePostMutation.mutate(id)
+        console.log(id)
+    }
+
     if(isLoading) return <h2>loding...</h2>
     if(isError) return <h2>{error.message}</h2>
 
@@ -25,7 +41,7 @@ function PostList()
                     return <div key={post.id} style={{background : '#777'}}>
                         <h4 style={{cursor:"pointer"}} onClick={() => navigate(`post/${post.id}`)}>{post.title}</h4>
                         <button onClick={() => navigate(`post/${post.id}/edit`)}>Edit</button>
-                        <button>Delete</button>
+                        <button onClick={() => handleDelete(post.id)}>Delete</button>
                     </div>
                 })
             }
